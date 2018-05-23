825692b2cace99ef92b5785cd2784be9cdd04ed4
DrManhattan666 <38150050+DrManhattan666@users.noreply.github.com>
2018-05-22 21:55:49 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'MyManifesto',
+  packages = ['MyManifesto'], 
+  version = '0.1',
+  description = 'Los principios por los que me he de regir',
+  author = 'Elias Garza',
+  author_email = 'matematicas.grupo8@gmail.com',
+  url = 'https://github.com/DrManhattan666/MyManifesto', 
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', 
+  keywords = ['zen', 'sos', 'example', 'philosophy'], 
+  classifiers = [],
+)

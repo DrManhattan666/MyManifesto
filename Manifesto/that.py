bc774f0feaaf497c6b1c655d271d34daf0b61801
DrManhattan666 <38150050+DrManhattan666@users.noreply.github.com>
2018-05-22 22:28:22 -0500

    Add files via upload

---
+++ Manifesto/that.py
@@ -0,0 +1,19 @@
+s = """Mi Manifiesto
+
+1. Ornhgvshy vf orggre guna htyl.
+2. Rkcyvpvg vf orggre guna vzcyvpvg.
+3. Fvzcyr vf orggre guna pbzcyrk.
+4. Pbzcyrk vf orggre guna pbzcyvpngrq.
+5. Syng vf orggre guna arfgrq.
+6. Fcnefr vf orggre guna qrafr.
+7. Ernqnovyvgl pbhagf.
+8. Fcrpvny pnfrf nera'g fcrpvny rabhtu gb oernx gur ehyrf.
+9. Nygubhtu cenpgvpnyvgl orngf chevgl.
+10. Reebef fubhyq arire cnff fvyragyl.
+11. ¡Sigue programando!.
+
+Por (y para) Elias,
+CaptainCisco (PyPI & TestPyPI)
+DrManhattan666 (GitHub)"""
+
+print(s)

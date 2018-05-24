76ec53499e27bd59e2dc37d1e7a48a1e27d64b82
DrManhattan666 <38150050+DrManhattan666@users.noreply.github.com>
2018-05-23 21:26:16 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Elias Garza',
   author_email = 'matematicas.grupo8@gmail.com',
   url = 'https://github.com/DrManhattan666/MyManifesto', 
-  download_url = 'https://github.com/peterldowns/MyManifesto/archive/0.1.tar.gz', 
+  download_url = 'https://github.com/DrManhattan666/MyManifesto/archive/0.1.tar.gz', 
   keywords = ['zen', 'sos', 'example', 'philosophy'], 
   classifiers = [],
 )
